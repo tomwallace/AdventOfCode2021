8b2f7d8e6daf21d98584f86977b4bc97d40680b6
Tom Wallace <thomaspwallace@gmail.com>
2021-12-07 08:17:04 -0500

    Adding Day 7 puzzle solutions and tests.

---
+++ AdventOfCode2021.Tests/DaySevenTests.cs
@@ -0,0 +1,47 @@
+﻿using AdventOfCode2021.Seven;
+using Xunit;
+
+namespace AdventOfCode2021.Tests;
+
+public class DaySevenTests
+{
+    [Fact]
+    public void FindFuelCostsForAllPositions()
+    {
+        var sut = new DaySeven();
+        var result = sut.FindFuelCostsForAllPositions(DaySeven.TestInput, false);
+
+        Assert.Equal(37, result[2]);
+        Assert.Equal(41, result[1]);
+        Assert.Equal(39, result[3]);
+        Assert.Equal(71, result[10]);
+    }
+
+    [Fact]
+    public void FindFuelCostsForAllPositions_WithProgressiveFuelIncreases()
+    {
+        var sut = new DaySeven();
+        var result = sut.FindFuelCostsForAllPositions(DaySeven.TestInput, true);
+
+        Assert.Equal(206, result[2]);
+        Assert.Equal(168, result[5]);
+    }
+
+    [Fact]
+    public void PartA_Actual()
+    {
+        var sut = new DaySeven();
+        var result = sut.PartA();
+
+        Assert.Equal("336131", result);
+    }
+
+    [Fact]
+    public void PartB_Actual()
+    {
+        var sut = new DaySeven();
+        var result = sut.PartB();
+
+        Assert.Equal("92676646", result);
+    }
+}
---
+++ AdventOfCode2021/Seven/CrabSub.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode2021.Seven;
+
+public class CrabSub
+{
+    public CrabSub(int location)
+    {
+        Location = location;
+    }
+
+    public int Location { get; }
+
+    public int MoveCostInFuel(int targetLocation, bool applyFuelStepIncrease)
+    {
+        if (!applyFuelStepIncrease)
+            return Math.Abs(targetLocation - Location);
+
+        // Fuel cost increases by 1 for every step taken
+        var numberOfSteps = Math.Abs(targetLocation - Location);
+        var stepCosts = Enumerable.Range(0, numberOfSteps).ToArray();
+        return numberOfSteps + stepCosts.Sum();
+    }
+}
